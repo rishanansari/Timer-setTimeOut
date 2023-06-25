a3aa8ce69495029fc021862c252838b489fc816f
ABID ANSARI <89006248+rishanansari@users.noreply.github.com>
2023-06-25 14:13:06 +0530

    script updated

--- timer.js
+++ timer.js
@@ -1,6 +1,6 @@
 //select an element h1 with 
 const countdown=document.querySelector(".countDown")
-
+const emojee = document.querySelector(".emojee");
 let time=10;//initalized variable with initail as 5
 let delay = 1000; // timer delay by 1000ms;
 
@@ -28,6 +28,7 @@ setTimeout(() => {
                                     setTimeout(() => {
                                         countdown.innerHTML=time--;
                                         countdown.innerHTML= "Hey Folks!";
+                                        emojee.innerHTML="<h1>🚀Here We Go...</h1>"
                                     }, delay);
                                 }, delay);
                             }, delay);
